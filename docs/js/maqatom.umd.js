7b5c4bb567ad05bd12079e1118d9446b8c16f3b8
td-shi <tisiphone.ready@gmail.com>
2022-09-15 21:21:41 +0900

    add.

--- docs/js/maqatom.umd.js
+++ docs/js/maqatom.umd.js
@@ -25,18 +25,18 @@
   function atomHr(){ return h('hr', {key: '' + genShortUniq()}, []);}
   function atomBr(){ return h('br', {key: '' + genShortUniq()}, []);}
   function atomText(text, id, classes = {}, style = ''){
-    return h('span#' + id), {classes: classes, style: style}, [text]);
+    return h('span#' + id, {classes: classes, style: style}, [text]);
   };
   function atomTextButton(text, id, onClickedFunc, classes = {}, style = ''){
     return h('button#' + id, {classes: classes, style: style, onclick: onClickedFunc}, [text]);
   };
   function atomLabelCheckbox(text, id, onChangedFunc, classes = {}, style = ''){
-    return h('label', {key: '' + id + 'Label', classes: classes, style: style, onchange: onChangedFunc,}, [
+    return h('label', {key: '' + id + 'Label', classes: classes, style: style, onchange: onChangedFunc}, [
       h('input#' + id, {type: 'checkbox'}, [text])
     ]);
   };
   function atomInputNumber(id, onChangeFunc, classes = {}, style = '', min = -9999.9999, max = 9999.9999, step = 'any'){
-    return h('input#' + id), {type: 'number', classes: classes, style: style, onchange: onChangeFunc, min: min, max: max, step: step}, []);
+    return h('input#' + id, {type: 'number', classes: classes, style: style, onchange: onChangeFunc, min: min, max: max, step: step}, []);
   };
   function atomCombobox(id, opt = [{val:0, text: ''}], onSelectFunc, classes = {}, style =  ''){
     var mapping = m(function getKey(x){return x;},
